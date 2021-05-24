fc1ad5964ee9ce7d4a6e9a26c82c432eca9aea48
veenith123 <veenithdora@gmail.com>
2021-05-24 20:45:37 +0530

    h

---
+++ kitequestion/js/questions.js
@@ -0,0 +1,62 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What is the poem about?",
+    answer: "A. Kite",
+    options: [
+      "A. Kite",
+      "B. Ballon"
+    ]
+  },
+    {
+    numb: 2,
+    question: "In which situation is the kite more active?",
+    answer: "B. In the air",
+    options: [
+      "A. On the ground",
+      "B. In the air"
+    ]
+  },
+    {
+    numb: 3,
+    question: "A Kite on the ground is like a",
+    answer: "B. Dead",
+    options: [
+      "A. Alive",
+      "B. Dead"
+    ]
+  },
+    {
+    numb: 4,
+    question: "How many stanzas are there in the poem",
+    answer: "A. 2",
+    options: [
+      "A. 2",
+      "B. 4"
+    ]
+  },
+    {
+    numb: 5,
+    question: "A Kite is made of _______ & _______.",
+    answer: "B. String & Paper",
+    options: [
+      "A. Pen & Paper",
+      "B. String & Paper"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
